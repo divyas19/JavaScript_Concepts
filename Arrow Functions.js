1f590adb8898f19d67adc081a97e1542eed52f52
Divya Suman <73398403+divyas19@users.noreply.github.com>
2023-05-01 19:30:28 +0530

    Create Arrow Functions.js

---
+++ Arrow Functions.js
@@ -0,0 +1,12 @@
+const greet = function()
+{
+    return "Good Morning";
+}
+
+// Examples of Arrow Function
+
+const greet = () =>  "Good Morning";
+
+const greet = () =>  ({name: "harry"});
+
+let myFunction = (a, b) => a * b;
